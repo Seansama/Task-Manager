d8838cd212868a5e738d881a75b1720479d74f8d
Shaun <seansama@pop-os.localdomain>
2023-02-28 13:50:54 +0300

    Recreated migration file correctly

---
+++ db/migrate/20230228105004_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.integer :id
+      t.text :description
+      t.date :due_date
+      t.boolean :status
+    end
+  end
+end
