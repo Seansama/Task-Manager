713fdf830c369a9846c39a902d8e58414b42ec6f
Shaun <seansama@pop-os.localdomain>
2023-02-28 13:58:35 +0300

    Successful DB migration

--- db/migrate/20230228105510_create_tasks.rb
+++ db/migrate/20230228105510_create_tasks.rb
@@ -1,7 +1,6 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.integer :id
       t.text :description
       t.date :due_date
       t.boolean :status
